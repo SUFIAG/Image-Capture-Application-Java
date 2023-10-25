d80e390b3a713147f9cfa7585da7ca2bcc96db3d
Sufyan Abdul Ghani <40076204+SUFIAG@users.noreply.github.com>
2023-10-26 02:01:40 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ImageCapture_App_JAVA
+
+### An Android application that uses the camera of an Android device to capture an image and show it inside the application. The code is written in Java Programming Language.
+- The image is stored in the object of the Bitmap class.
---
+++ app/src/main/java/com/example/imagecapture_app_java/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.imagecapture_app_java;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button cameraButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cameraButton = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+
+        cameraButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, 0);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+
+    }
+}
